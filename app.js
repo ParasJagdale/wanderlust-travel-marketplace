004b385fe2ee9d679744660dec2356fcfc605c1d
Paras Jagdale <parasjagdale15@gmail.com>
2025-07-21 19:44:00 +0530

    Added the port and url in env

--- app.js
+++ app.js
@@ -108,7 +108,8 @@ app.use((err, req, res, next) => {
     // res.status(statusCode).send(message);
 });
 
-app.listen(8080, () => {
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
     console.log("server is listening to port 8080");
 });
 
